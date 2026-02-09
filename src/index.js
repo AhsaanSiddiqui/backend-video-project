904fc1a9724ec289b8aa779d53a3d727db3607e2
Ahsan Zafar <siddiquiahsan127@gmail.com>
2026-02-10 01:18:01 +0500

    utils setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 // require('dotenv').config({ path: './.env' });
 import dotenv from 'dotenv';
 import { connectDB } from './db/index.js';
+import app from './app.js';
 
 dotenv.config({ path: './.env' });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on('error', (error) => {
+      console.error('Server error: ', error);
+      throw error;
+    });
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log('Error in DB Connection Failed: ', error);
+  });
+
+//
