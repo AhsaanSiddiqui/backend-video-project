0b041f2f9433d23f21f101f806610d1d8df5b71b
Ahsan Zafar <siddiquiahsan127@gmail.com>
2026-02-11 19:50:17 +0500

    all setup configured successfully

--- src/app.js
+++ src/app.js
@@ -19,4 +19,12 @@ app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
+// routes import
+import userRouter from './routes/user.routes.js';
+
+// routes declaration
+app.use('/api/v1/users', userRouter);
+
+// http://localhost:8000/api/v1/users/register
+
 export default app;
